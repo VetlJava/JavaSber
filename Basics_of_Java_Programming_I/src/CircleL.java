5d046f9fe0dcfe78db131bb7b181711896a59dc8
Vitalii Metlinov <vetl.foto@gmail.com>
2023-02-22 19:25:31 +0300

    added gitignore

--- Basics_of_Java_Programming_I/src/CircleL.java
+++ Basics_of_Java_Programming_I/src/CircleL.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CircleL {
     public static void main(String[] args) {
         final double PI = 3.14159;
-        
+
         double L, radius;
 
         Scanner input = new Scanner(System.in);
