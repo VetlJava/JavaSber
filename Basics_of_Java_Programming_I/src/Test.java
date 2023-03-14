dd30cfdfdf1e96ceffbd1c5841e6cbde078a92a3
VetlJava <vetl.foto@gmail.com>
2023-03-15 00:17:06 +0300

    ComputeAngles - градусы углов трецгольника через вершины

--- Basics_of_Java_Programming_I/src/Test.java
+++ Basics_of_Java_Programming_I/src/Test.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-        int x = 11, y = 1 ;
-        System.out.println(x > 0 && x < 10);
-        System.out.println((x > 0) && (x < 10));
+        int x = 10, y = -1, z = 1;
+//        System.out.println(x > 0 && x < 10);
+//        System.out.println((x > 0) && (x < 10));
+//
+//        System.out.println(x > 0 || x < 10);
+//        System.out.println((x > 0) || (x < 10));
+//
+//        System.out.println(x > 0 || x < 10 && y < 0);
+//        System.out.println(x > 0 || (x < 10 && y < 0));
+       // System.out.println(1+1+1+1+1 == 5);
 
-        System.out.println(x > 0 || x < 10);
-        System.out.println((x > 0) || (x < 10));
+        System.out.println(x != y);
+        System.out.println(!(x == y));
+        System.out.println(x > y && x < y);
+        System.out.println();
 
-        System.out.println(x > 0 || x < 10 && y < 0);
-        System.out.println(x > 0 || (x < 10 && y < 0));
+
     }
 }
