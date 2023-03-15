be16f55945614bb7efc4af011aed85301b1006f1
VetlJava <vetl.foto@gmail.com>
2023-03-15 12:47:35 +0300

    StriingExample1 -  работа со строками. Часть 1

--- Basics_of_Java_Programming_I/src/Test.java
+++ Basics_of_Java_Programming_I/src/Test.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
 
-        int x = 10, y = -1, z = 1;
 //        System.out.println(x > 0 && x < 10);
 //        System.out.println((x > 0) && (x < 10));
 //
@@ -14,10 +13,25 @@ public class Test {
 //        System.out.println(x > 0 || (x < 10 && y < 0));
        // System.out.println(1+1+1+1+1 == 5);
 
-        System.out.println(x != y);
-        System.out.println(!(x == y));
-        System.out.println(x > y && x < y);
-        System.out.println();
+//        System.out.println(x != y);
+//        System.out.println(!(x == y));
+//        System.out.println(x > y && x < y);
+//        System.out.println((int) '1');
+//        System.out.println((int) 'A');
+//        System.out.println((int) 'a');
+//
+//        System.out.println((char) 40);
+//        System.out.println((char) 90);
+//        System.out.println((char) 0X90);
+
+//        int x = 'a';
+//        System.out.println(++x);
+//        int c = 'A';
+//        int i = (int) c;
+
+
+
+
 
 
     }
