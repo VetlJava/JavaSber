2ab6e793f72b546861aee90d4e729b07a1843560
VetlJava <vetl.foto@gmail.com>
2023-04-24 11:47:38 +0300

    add gitignore

--- Basics_of_Java_Programming_I/src/Test.java
+++ Basics_of_Java_Programming_I/src/Test.java
@@ -28,10 +28,35 @@ public class Test {
 //        System.out.println(++x);
 //        int c = 'A';
 //        int i = (int) c;
+        String s1 = "Welcome";
+        String s2 = "welcome";
 
+        boolean isEqual = s1.equals(s2);
+        System.out.println("1. " + isEqual);
 
+        isEqual = s1.equalsIgnoreCase(s2);
+        System.out.println("2. " + isEqual);
 
+        int x = s1.compareTo(s2);
+        System.out.println("3. x: " + x);
 
+        x = s1.compareToIgnoreCase(s2);
+        System.out.println("4. " + x);
+
+        boolean b = s1.startsWith("AAA");
+        System.out.println("5. префикс b: " + b);
+
+        b = s1.endsWith("AAA");
+        System.out.println("6. суфикс b: " + b);
+
+        x = s1.length();
+        System.out.println("7. "  + x);
+
+        x = (char) s1.charAt(0);
+        System.out.println("8. " + x);
+
+        String s3 = s1 + s2;
+        System.out.println("9. " + s3);
 
 
     }
