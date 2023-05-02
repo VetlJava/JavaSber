4a81896b4ac90fcbdce3e10ffc3174776b92a832
VetlJava <vetl.foto@gmail.com>
2023-05-02 11:54:01 +0300

    ПРОЕКТ Часть 3. ConvertAmoutn добавленый циклы

--- src/project/ConvertAmount.java
+++ src/project/ConvertAmount.java
@@ -7,29 +7,35 @@ import java.util.Scanner;
 public class ConvertAmount {
     public static void main(String[] args) {
         final double ROUBLES_PER_DOLLAR = 72.12; //курс покупки
-        int dollars, digit;
+        int dollars, digit, n, i;
         double roubles;
 
         Scanner input = new Scanner(System.in); //Получить сумму денег в американских долларах
 
-        System.out.println("Введите сумму денег в американских долларах: ");
-        dollars = input.nextInt();
-        System.out.print(dollars);
+        do {
+            System.out.print("Введите корректное количество конвертаций: ");
+            n = input.nextInt();
+        } while (n <= 0);
 
-        if (dollars >= 5 && dollars <= 20) {
-            System.out.println(" американских долларов равны");
-        } else {
-            digit = dollars % 10;
-            if (digit == 1) {
-                System.out.println(" американский доллар равен");
-            } else if (digit >= 2 && digit <= 4) {
-                System.out.println(" американских доллара равны");
-            } else System.out.println(" американских долларов равны");
+        for (i = 1; i <= n; i++) {
+            System.out.print("Введите сумму денег в американских долларах: ");
+            dollars = input.nextInt();
+            System.out.print(dollars);
+
+            if (dollars >= 5 && dollars <= 20) {
+                System.out.println(" американских долларов равны");
+            } else {
+                digit = dollars % 10;
+                if (digit == 1) {
+                    System.out.println(" американский доллар равен");
+                } else if (digit >= 2 && digit <= 4) {
+                    System.out.println(" американских доллара равны");
+                } else System.out.println(" американских долларов равны");
+            }
+
+            roubles = ROUBLES_PER_DOLLAR * dollars;
+
+            System.out.print((int) (roubles * 100) / 100.0 + " российского рубля.\n");
         }
-
-        roubles = ROUBLES_PER_DOLLAR * dollars;
-
-        System.out.print((int)(roubles * 100) / 100.0 + " российского рубля.");
-
     }
 }
